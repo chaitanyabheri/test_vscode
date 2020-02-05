eea72be736d49bbcfd89de63915bc559429b676e
chaitanya bheri <chaitanya.bheri@procore.com>
2020-02-05 02:48:29 -0600

    initial commit

---
+++ test.py
@@ -0,0 +1,21 @@
+import math
+import os
+import sys
+from os import rename
+
+import requests
+
+a = 1
+b = 2
+
+print ("sum= ", a + b)
+test = "test"
+print asda
+
+
+def greet(who_to):
+    greeting = "Hello {}".format(who_to)
+
+
+return greeting
+
